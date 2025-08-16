8bcd3a2a502cf9376bea5d1bc4016fbad0c3ee47
bramar2 <bramar224@gmail.com>
2025-08-16 16:32:56 +0700

    action page

--- scripts/action-page.js
+++ scripts/action-page.js
@@ -1,6 +1,22 @@
-for(const input of document.querySelectorAll('.option > input')) {
-    const id = input.id;
-    input.addEventListener('change', (e) => {
-        console.log("Change", id);
+function saveSettings() {
+    let config = {};
+    for(const input of document.querySelectorAll('.option > input')) {
+        config[input.id] = input.checked;
+    }
+    chrome.storage.sync.set(config);
+}
+
+function loadSettings() {
+    const DEFAULT_CONFIG = {
+        "config-premium-elements": true
+    };
+    chrome.storage.sync.get(DEFAULT_CONFIG, (config) => {
+        for(const key of Object.keys(DEFAULT_CONFIG)) {
+            const element = document.getElementById(key);
+            element.checked = config[key];
+            element.addEventListener('change', saveSettings);
+        }
     });
 }
+
+document.addEventListener('DOMContentLoaded', loadSettings);
